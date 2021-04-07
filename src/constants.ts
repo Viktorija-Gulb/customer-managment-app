b7d3fb902c5f24649aedb60e8518b5c39f7f2ec8
vikgulb <vikgulb@gmail.com>
2021-04-07 20:31:04 +0300

    remove api key

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const BASE_URL='https://maps.googleapis.com/maps/api/geocode/json';
-export const API_KEY='AIzaSyBQMdwglpSVgVP-f332VhaTg43Mket4sf0';
+export const API_KEY=''; // įrašyti api key
 
 
 export const tableHeader = [
